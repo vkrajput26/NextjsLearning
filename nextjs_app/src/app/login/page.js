f5294555f15ddb19162940145f6d300bf67079f6
vivek_rajput <indvivekrajput@gmail.com>
2023-10-06 15:09:19 +0530

    implementing routing

---
+++ nextjs_app/src/app/login/page.js
@@ -0,0 +1,19 @@
+
+'use client'
+import { useRouter } from "next/navigation"
+
+ const Login =()=>{
+  
+    const router=useRouter()
+    const rountingfun=(name)=>{
+        router.push(name)
+    }
+    return(
+        <div>
+            <h1> Login  page</h1>
+            <button onClick={()=>rountingfun('/')} >Home</button>
+        </div>
+    )
+}
+
+export default Login
--- nextjs_app/src/app/page.js
+++ nextjs_app/src/app/page.js
@@ -3,13 +3,20 @@
 
 import Image from 'next/image'
 import styles from './page.module.css'
+import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  const router = useRouter()
+  
   return (
     <main className={styles.main}>
        <p>home</p>
        <button onClick={()=>alert('hello')} >alert</button>
+        <Link href='/login'>Go to login page</Link>
+        <button onClick={()=>router.push('/login')} >About page</button>
+    
     </main>
   )
 }
- 
+  
